79d38831e59200ec007706923c086fdc103a498d
Eric Vuong <ericvuong22222@gmail.com>
2018-01-28 01:14:50 -0500

    Visual Recognition module implemented, TODO: API Nutrition module integration

--- CANiEatThis/app/src/main/java/com/hackathon/conuhacks/canieatthis/MainActivity.java
+++ CANiEatThis/app/src/main/java/com/hackathon/conuhacks/canieatthis/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Debug;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -17,15 +18,25 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.*;
 import java.util.Date;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.json.*;
+import java.util.List;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.*;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.*;
 import static android.R.attr.duration;
+import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     String mCurrentPhotoPath;
     ImageView mImageView;
     Button mNewImageButton;
+    TextView mTextView;
+    Button mSetProfile;
 
 
     @Override
@@ -44,14 +57,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        mTextView = (TextView) findViewById(R.id.mHelloWorldText);
         mOpenCamera = (Button) findViewById(R.id.mCameraButton);
         mImageView = (ImageView) findViewById(R.id.pictureTaken);
         mNewImageButton = (Button) findViewById(R.id.newImage);
+        mSetProfile = (Button) findViewById(R.id.mSetProfileButton);
+        SetProfile();
         OpenCamera();
         SetNewImage();
 
     }
 
+    public void SetProfile(){
+        mSetProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });}
     public void SetNewImage(){
         mNewImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,7 +89,17 @@ public class MainActivity extends AppCompatActivity {
                     Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
                     mImageView.setImageBitmap(myBitmap);
 
+
                 }
+                try {classifyImage(mCurrentPhotoPath);}
+                catch(Exception e){
+                    e.printStackTrace();
+                    for(StackTraceElement elem : e.getStackTrace()){
+                        Log.e("gg",elem.toString());
+                    }
+                    Log.e("aha",Log.getStackTraceString(e));
+                    (Toast.makeText(getApplicationContext(),e.getStackTrace().toString(),Toast.LENGTH_SHORT)).show();
+                };
             }
         });
     }
@@ -114,5 +150,41 @@ public class MainActivity extends AppCompatActivity {
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
     }
+    private String classifyImage(String path)throws IOException, JSONException{
 
+
+            VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+
+            service.setEndPoint("https://gateway-a.watsonplatform.net/visual-recognition/api");
+            service.setApiKey("072ba1a08e2c4d39ec1d9fcdfe8b2a8f50ddcb54");
+
+            //(hard-coded filed name) my_images.jpg
+
+            InputStream imagesStream = new FileInputStream(path);
+
+
+            ClassifyOptions classifyOptions = new ClassifyOptions.Builder().imagesFile
+                    (imagesStream).imagesFilename(path).parameters("{\"classifier_ids\": [\"food\"]}")
+                    .build();
+
+            ClassifiedImages result = service.classify(classifyOptions).execute();
+
+            Log.d("help",result.toString());
+
+            JSONObject obj = new JSONObject(result.toString());
+            //Getting first result
+
+            JSONArray arr = obj.getJSONArray("images").getJSONObject(0).getJSONArray("classifiers").getJSONObject(0).getJSONArray("classes");
+            int size = arr.length();
+
+            JSONObject res = obj.getJSONArray("images").getJSONObject(0).getJSONArray("classifiers").getJSONObject(0).getJSONArray("classes").getJSONObject(0);
+            String name = res.getString("class");
+            mTextView.setText(name);
+
+            Toast toast = Toast.makeText(this, name, Toast.LENGTH_SHORT);
+            toast.show();
+
+            return name;
+
+    }
 }
